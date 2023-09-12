9a4721bb8110ec164d9890fd5f181dedb7c51464
eccentric <zhosan00@list.ru>
2023-09-12 14:19:05 +0300

    new

--- untitled/src/main/java/org/example/Configure.java
+++ untitled/src/main/java/org/example/Configure.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configure {
+
+    @Bean
+    Cat cat() {
+        Cat cat = new Cat();
+        cat.setName("Василий");
+
+        return cat;
+    }
+    
+
+    Person person() {
+        Person person = new Person();
+        person.setName("Дмитрий");
+
+        return person
+    }
+
 }
