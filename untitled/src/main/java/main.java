5192135a7d8f8acc9abe3a42cd54b5f9a507ab79
eccentric <zhosan00@list.ru>
2023-09-12 14:09:41 +0300

    new

--- untitled/src/main/java/main.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class main {
-    public static void main(String[] args) {
-
-        var context = new AnnotationConfigApplicationContext(Configure.class);
-
-    }
-}
---
+++ untitled/src/main/java/org/example/Person.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Person {
+    private String name;
+
+    Cat cat;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
