b756cf76dd1d787d80d96fedc33e904681a4e394
eccentric <zhosan00@list.ru>
2023-09-12 14:40:44 +0300

    problem

--- untitled/src/main/java/org/example/Configure.java
+++ untitled/src/main/java/org/example/Configure.java
@@ -1,25 +1,28 @@
 package org.example;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@ComponentScan(basePackages = "Main")
 public class Configure {
 
-    @Bean
-    Cat cat() {
-        Cat cat = new Cat();
-        cat.setName("Василий");
-
-        return cat;
-    }
-
-    @Bean
-    Person person() {
-        Person person = new Person();
-        person.setName("Дмитрий");
-
-        return person;
-    }
+//    @Bean
+//    Cat cat() {
+//        Cat cat = new Cat();
+//        cat.setName("Василий");
+//
+//        return cat;
+//    }
+//
+//    @Bean
+//    Person person() {
+//        Person person = new Person();
+//        person.setName("Дмитрий");
+//
+//        return person;
+//    }
 
 }
--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
 
 public class Main {
     public static void main(String[] args) {
 
         var context = new AnnotationConfigApplicationContext(Configure.class);
 
-        var person = context.getBean(Person.class);
+
         var cat = context.getBean(Cat.class);
 
-
-        System.out.println(person.getName() + "\n" + cat.getName());
+        cat.setName("Василий");
+        System.out.println(cat.getName());
 
 
     }
