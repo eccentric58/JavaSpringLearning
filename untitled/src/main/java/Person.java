0f1a8a87695f8ba6ac1c9ddd710c2dd3d6ae12eb
eccentric <zhosan00@list.ru>
2023-09-12 13:48:48 +0300

    test

---
+++ untitled/src/main/java/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+
+    private String name;
+
+
+    Cat cat;
+
+
+
+}
---
+++ untitled/src/main/java/main.java
@@ -0,0 +1,9 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class main {
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(Configure.class);
+
+    }
+}
